91786056435af3f564bf167ac8b2bb1563ea7a37
neil <neil@abstractsec.com>
2018-06-22 22:35:53 -0400

    complete distract_the_guards

--- 4_distract_the_guards/solution.py
+++ 4_distract_the_guards/solution.py
@@ -1,18 +1,36 @@
+def is_infinite(x, y):
+    """ Determind is the pair will play indefinitly
+    """
+    # sort x and y such that x < y
+    (x, y) = (x, y) if x < y else (y, x)
+    while x != y:
+        # x wins while x < y
+        y -= x
+        x *= 2
+
+        # if the numbers x becomes larger than y, we will keep playing indefitely
+        if x > y:
+            return True
+    return False
+
 def answer(banana_list):
+    """ With the given banana counts, determine how many guards will not be paired off indefinitly
+    """
     guards = len(banana_list)
-    # find combinations that have an odd difference
-    paired = []
-    for i in range(len(banana_list)):
+    # find combinations that will play infinitley
+    paired = ()
+    for i in range(guards-1):
         if i in paired:
             continue
-        x = banana_list[i]
-        for j in range(i+1, len(banana_list)):
-            y = banana_list[j]
-            if (y-x) > 0 and (y-x) % 2 == 0:
-                paired += [x, y]
-                is_paired = True
+        x = int(banana_list[i])
+        for j in range(i+1, guards):
+            if j in paired:
+                continue
+            y = int(banana_list[j])
+            if is_infinite(x, y):
+                paired += (i, j)
                 break
-    
+
     # calculate the number of unpaired guards
     return guards - len(paired)
 
@@ -24,5 +42,8 @@ def test(expect, l):
 
 
 if __name__ == "__main__":
+    test(1, [1])
     test(2, [1, 1])
     test(0, [1, 7, 3, 21, 13, 19])
+    test(1, [3, 3, 2, 6, 6])
+    test(0, [1, 2, 1, 7, 3, 21, 13, 19])
