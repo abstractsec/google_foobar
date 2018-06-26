c953648fbb54aec5400b94ec1711f20f3c792161
neil <neil@abstractsec.com>
2018-06-25 20:01:05 -0400

    less efficient, but more complete solution

---
+++ 4_running_with_bunnies/solution2.py
@@ -0,0 +1,86 @@
+""" Rescue as many bunnies as possible
+
+    This is a graph problem where you start at the initital node and want to visit the maximum
+    number of unique inner nodes, while still reaching the final node before your timer expires.
+    The cost of traveling from one node to another is asymmetric and may be negative (increasing
+    your timer)
+
+    This solution is less efficient that the previous one, becuase it evaluates all permutations
+    of a given 
+
+    Passing 9/10 tests
+"""
+import itertools
+
+def validate_path(times, path, ttl):
+    """validate that the given path between stages can be visited in the given ttl
+    """
+    full_path = [p for p in path] + [len(times)-1]
+
+    prev = 0
+    path_cost = 0
+    for s in full_path:
+        path_cost += times[prev][s]
+        prev = s
+    
+    return ttl >= path_cost
+
+def optimize_graph(times):
+    """ implementation of floyd-warshall to find shortest path from node to node
+    """
+    dist = times
+    verticies = len(times)
+    for k in range(verticies):
+        for i in range(verticies):
+            for j in range(verticies):
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+    return dist
+
+def answer(times, time_limit):
+    """ Determine which nodes you can use to visit the maximum number of unique in nodes
+    """
+    target = len(times) - 1
+    optimal_times = optimize_graph(times)
+    ttl = time_limit
+
+    # visit as manny inner nodes as possible before timer expires
+    path = []
+    bunnies = range(1, target)
+    for cnt in range(1, target):
+        min_sum = 1000000
+        this_path = None
+        for p in itertools.permutations(bunnies, cnt):
+            p_sum = sum(p)
+            if p_sum < min_sum and validate_path(optimal_times, p, ttl):
+                min_sum = p_sum
+                this_path = [x-1 for x in sorted(p)]
+        if this_path:
+            path = this_path
+        else:
+            break
+
+    return path
+
+
+def test(expect, m, time_limit):
+    expect = sorted(expect)
+    s = answer(m, time_limit)
+    print "PASS " if s == expect else "FAIL ",
+    print "%d @ %s -> %s" % (time_limit, m, s)
+
+
+if __name__ == "__main__":
+    test([1, 2], [
+        [0, 2, 2, 2, -1],
+        [9, 0, 2, 2, -1],
+        [9, 3, 0, 2, -1],
+        [9, 3, 2, 0, -1],
+        [9, 3, 2, 2, 0]
+        ], 1)
+
+    test([0], [
+        [0, 2, 2, -1],
+        [9, 0, 8, 1],
+        [9, 8, 0, 1],
+        [9, 1, 1, 0]
+        ], 1)
